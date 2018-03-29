b90ed6dd80615d0c88bb333a95081d27a3d41d22
Manon Barthelemy <manon.barthelemy@gadz.org>
2018-03-29 00:47:49 -0400

    we can print!

--- src/components/CreateSequence.jsx
+++ src/components/CreateSequence.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import GridSequence from './GridSequence';
+import SequencePrint from './SequencePrint';
 import PoseSelector from './PoseSelector';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 
 let style = {
@@ -75,12 +78,36 @@ let selectedPosesSource = [
     id: "three",
     name: "picture_three"
   }];
+
+function sleep (time) {
+  console.log()
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 class CreateSequence extends Component {
   constructor(props) {
     super(props);
-    this.state = {selected_poses: selectedPosesSource};
+    this.state = {
+      selected_poses: selectedPosesSource,
+      isPrintable: false
+    };
 
     this.handlePoseSelectionChange = this.handlePoseSelectionChange.bind(this);
+    this.print = this.print.bind(this);
+    this.resetIsPrintable = this.resetIsPrintable.bind(this);
+  }
+
+  resetIsPrintable() {
+    this.setState({isPrintable:false});
+  }
+
+  print(){
+    console.log(this.state);
+    this.setState({isPrintable: true});
+    sleep(50).then(()=>{
+      window.print();
+      window.onafterprint = this.resetIsPrintable();
+    });
   }
 
   handlePoseSelectionChange (e) {
@@ -90,26 +117,33 @@ class CreateSequence extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <p> Yo sequence time! </p>
-        <table style={style.table}>
-          <tbody>
-            <tr>
-              <td style={style.td}>
-                <PoseSelector
-                  handlePoseSelection= {(e) => this.handlePoseSelectionChange(e)}
-                  grouped_poses = {grouped_poses}
-                />
-              </td>
-              <td>
-                <GridSequence selectedPoses={this.state.selected_poses} />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
+
+    let content = this.state.isPrintable ?
+      (<SequencePrint  selected_poses={this.state.selected_poses}/>) :
+      (
+        <div>
+          <MuiThemeProvider>
+            <RaisedButton label="Print" onClick={() => this.print()} />
+          </MuiThemeProvider>
+          <p> Yo sequence time! </p>
+          <table style={style.table}>
+            <tbody>
+              <tr>
+                <td style={style.td}>
+                  <PoseSelector
+                    handlePoseSelection= {(e) => this.handlePoseSelectionChange(e)}
+                    grouped_poses = {grouped_poses}
+                  />
+                </td>
+                <td>
+                  <GridSequence selectedPoses={this.state.selected_poses} />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
+    return (<div>{content}</div>);
   }
 }
 
